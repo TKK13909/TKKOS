f490a131e1e8d1fdf9246b2e4a05e272501a7f1b
Krafty Kactus <tkk@tkk-fedora>
2026-05-24 11:00:15 -0700

    Clean up repo

--- iso-podman.sh
+++ iso-podman.sh
@@ -5,5 +5,3 @@ IMAGE_REPO=ghcr.io/tkk13909 \
 IMAGE_NAME=TKKOS \
 IMAGE_TAG=latest \
 VARIANT=Kinoite # should match the variant your image is based on
-
-# Test message that should not trigger a rebuild
